d15ea2340f4d67fcfb0f20e791982558e899a773
saz-im-netz <130750824+saz-im-netz@users.noreply.github.com>
2024-02-12 16:06:43 +0100

    optimierungen und start kontaktformular

--- index.js
+++ index.js
@@ -36,3 +36,41 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+
+// --- contact form
+
+const constraints = {
+  name: {
+      presence: { allowEmpty: false }
+  },
+  email: {
+      presence: { allowEmpty: false },
+      email: true
+  },
+  message: {
+      presence: { allowEmpty: false }
+  }
+}
+
+const form = document.getElementById('contact-form')
+
+form.addEventListener('submit', function (event) {
+const formValues = {
+    name: form.elements.name.value,
+    email: form.elements.email.value,
+    message: form.elements.message.value
+}
+
+const errors = validate(formValues, constraints)
+
+if (errors) {
+  event.preventDefault();
+  const errorMessage = Object
+      .values(errors)
+      .map(function (fieldValues) { return fieldValues.join(', ')})
+      .join("\n")
+
+  alert(errorMessage)
+}
+}, false)
